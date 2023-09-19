3cd8f1cfb4aedd1482ac39434e3dc781436a67d2
rilton bispo <riltondev@gmail.com>
2023-09-19 16:31:35 -0300

    feat: create home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Valorant Agents Wiki</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta delectus
+        fuga possimus natus laudantium eveniet expedita in nostrum reprehenderit
+        rerum. Atque non quod modi hic explicabo sint nam nesciunt facilis?
+      </p>
+      <Link href="/agents">Ver Agents</Link>
     </div>
   );
 }
