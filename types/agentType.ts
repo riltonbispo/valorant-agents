d343f671b7b39f6d1016b411fe2e92610ee4b05b
rilton bispo <riltondev@gmail.com>
2023-09-19 16:15:38 -0300

    refactor: agent type

--- types/agentType.ts
+++ types/agentType.ts
@@ -1,11 +1,37 @@
 export interface AgentType {
-  uuid: number;
+  uuid: string;
   displayName: string;
   description: string;
-  fullPortrait: string;
+  developerName: string;
+  characterTags: null | any[];
   displayIcon: string;
+  displayIconSmall: string;
+  bustPortrait: string;
+  fullPortrait: string;
+  fullPortraitV2: string;
+  killfeedPortrait: string;
+  background: string;
+  backgroundGradientColors: string[];
+  assetPath: string;
+  isFullPortraitRightFacing: boolean;
+  isPlayableCharacter: boolean;
+  isAvailableForTest: boolean;
+  isBaseContent: boolean;
   role: {
     uuid: string;
     displayName: string;
+    description: string;
+    displayIcon: string;
+    assetPath: string;
   };
+  recruitmentData: null | any;
+  abilities: Ability[];
+  voiceLine: null | any;
 }
+
+type Ability = {
+  slot: string;
+  displayName: string;
+  description: string;
+  displayIcon: string;
+};
