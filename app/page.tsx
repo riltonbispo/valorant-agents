7287d471a4bb9ea0f1ec3072497890397a646931
rilton bispo <riltondev@gmail.com>
2023-09-20 16:19:20 -0300

    style: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import style from "./page.module.css";
+import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Valorant Agents Wiki</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta delectus
-        fuga possimus natus laudantium eveniet expedita in nostrum reprehenderit
-        rerum. Atque non quod modi hic explicabo sint nam nesciunt facilis?
-      </p>
-      <Link href="/agents">Ver Agents</Link>
+    <div className={style.container}>
+      <div className={style.content}>
+        <h1 className={style.title}>Valorant Agents</h1>
+        <p className={style.description}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta
+          delectus fuga possimus natus laudantium eveniet expedita in nostrum
+          reprehenderit rerum. Atque non quod modi hic explicabo sint nam
+          nesciunt facilis?
+        </p>
+        <Link href="/agents" className={style.link}>
+          Ver Agents
+        </Link>
+      </div>
     </div>
   );
 }
