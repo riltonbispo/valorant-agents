5fb6293a4fbc66b56f4a511bd1047439731725d3
rilton bispo <riltondev@gmail.com>
2023-09-20 16:18:53 -0300

    style: agents page

--- app/agents/[slug]/page.tsx
+++ app/agents/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { getUnicAgent } from "@/services/api";
 import { AgentType } from "@/types/agentType";
 import React, { useEffect, useState } from "react";
 
+import style from "./page.module.css";
+
 const Agent = ({ params }: { params: { slug: string } }) => {
   const [agent, setAgent] = useState<AgentType | null>(null);
 
@@ -16,22 +18,34 @@ const Agent = ({ params }: { params: { slug: string } }) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <h2>{agent?.displayName}</h2>
-        <p>{agent?.description}</p>
+    <div className={style.container}>
+      <div className={style.content}>
+        <div className={style.info}>
+          <h2 className={style.info__title}>{agent?.displayName}</h2>
+          <p className={style.info__description}> {agent?.description}</p>
+        </div>
+
+        <div className={style.abilites}>
+          {agent?.abilities.map((abilite) => (
+            <div className={style.abilite__container}>
+              <img
+                src={abilite.displayIcon}
+                alt=""
+                className={style.abilite__image}
+              />
+              <div className={style.abilite__body}>
+                <h4 className={style.abilite__title}>{abilite.displayName}</h4>
+                <p className={style.abilite__description}>
+                  {abilite.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
+
+      <div className={style.image}>
         <img src={agent?.fullPortrait} alt="" />
-      </div>
-      <div>
-        {agent?.abilities.map((abilite) => (
-          <div>
-            <img src={abilite.displayIcon} alt="" />
-            <h4>{abilite.displayName}</h4>
-            <p>{abilite.description}</p>
-          </div>
-        ))}
       </div>
     </div>
   );
--- app/agents/page.tsx
+++ app/agents/page.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { getAgents } from "@/services/api";
 import { AgentType } from "@/types/agentType";
-import Card from "../components/Card";
+import Card from "../components/Card/card";
+import style from "./page.module.css";
 
 const page = () => {
   const [agents, setAgents] = useState<AgentType[]>([]);
@@ -16,16 +17,20 @@ const page = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Agentes Valorant</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore</p>
-      <ul>
-        {agents.map((agent) => (
-          <li key={agent.uuid}>
-            <Card agent={agent}></Card>
-          </li>
-        ))}
-      </ul>
+    <div className={style.container}>
+      <div className={style.content}>
+        <h1 className={style.title}>Agentes Valorant</h1>
+        <p className={style.description}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+        </p>
+        <ul className={style.list}>
+          {agents.map((agent) => (
+            <li key={agent.uuid} className={style.item}>
+              <Card agent={agent}></Card>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
